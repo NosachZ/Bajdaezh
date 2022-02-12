fca88738ecee29f8c464dcc371d34be6be3d424e
NOSACH\Zaharchenko <nosach.zaharchenko@gmail.com>
2022-02-12 19:28:14 +0200

    стартовая страница сайта

---
+++ script/main.js
@@ -0,0 +1,26 @@
+var feedbackIndex = 1;
+showFeedback(feedbackIndex);
+
+function listFeedback(n) {
+  showFeedback(feedbackIndex += n);
+}
+
+function currentFeedback(n) {
+  showFeedback(feedbackIndex = n);
+}
+
+function showFeedback(n) {
+  var i;
+  var feedbacks = document.getElementsByClassName("feedback-sample");
+  var dots = document.getElementsByClassName("slider-dot");
+  if (n > feedbacks.length) {feedbackIndex = 1}    
+  if (n < 1) {feedbackIndex = feedbacks.length}
+  for (i = 0; i < feedbacks.length; i++) {
+      feedbacks[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  feedbacks[feedbackIndex-1].style.display = "block";  
+  dots[feedbackIndex-1].className += " active";
+}
